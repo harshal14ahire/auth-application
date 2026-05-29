c87efc7d9e6f1738ae3ff9f9319a6ce530a95440
harshal14ahire <harshal14ahire@gmail.com>
2026-05-29 21:36:16 +0530

    chore: fix package typo in PersonalApplication

--- mfa-backend/src/main/java/passway/example/personal/PersonalApplication.java
+++ mfa-backend/src/main/java/passway/example/personal/PersonalApplication.java
@@ -1,4 +1,4 @@
-₹1package passway.example.personal;
+package passway.example.personal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
